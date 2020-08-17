1267c73f1fe0ac87e4367a9abd3232a9f75e5647
Smiley <43641640+PSGSmiley@users.noreply.github.com>
2020-08-17 16:02:05 +0200

    Update Interface_Sensors.cpp

--- Code/Code/Rescue-Bot/Interface_Sensors.cpp
+++ Code/Code/Rescue-Bot/Interface_Sensors.cpp
@@ -1,55 +1,121 @@
-
 #include <iostream>
-
+#include <string>
 using namespace std;
-
+char identify = 'R';
+int pos[3] ={28,3};
 class Sensor {
     public:
         virtual int getSensor() = 0;
-
 };
-class Sensorstring {
+class Sensorarray {
     public:
-        virtual string getSensorstring() = 0;
-
+        virtual int* getSensorarray() = 0;
 };
 
 class Ultraa: public Sensor {
     public:
         int getSensor(){
-            int data = 1;
-            return data;
+            int data = 0;
+            switch(identify){
+                case 'R':
+                    data = 1;
+                    return data;
+                case 'G':
+                    data = 0;
+                    return data;
+                case 'O':
+                    data = 1;
+                    return data;
+                break;
+                case 'W':
+                    data = 0;
+                    return data;
+                break;
+                default:
+                    data = 0;
+                    return data;
+                break;
+            }
         }
 };
 
 class Ultrab: public Sensor {
     public:
         int getSensor(){
-            int data = 1;
-            return data;
+            int data = 0;
+            switch(identify){
+                case 'R':
+                    data = 1;
+                    return data;
+                case 'G':
+                    data = 0;
+                    return data;
+                case 'O':
+                    data = 1;
+                    return data;
+                break;
+                case 'W':
+                    data = 0;
+                    return data;
+                break;
+                default:
+                    data = 0;
+                    return data;
+                break;
+            }
         }
 };
 
 class Wasser: public Sensor {
     public:
         int getSensor(){
-            int data = 1;
-            return data;
+            int data = 0;
+            switch(identify){
+                case 'R':
+                    data = 0;
+                    return data;
+                case 'G':
+                    data = 0;
+                    return data;
+                case 'O':
+                    data = 0;
+                    return data;
+                break;
+                case 'W':
+                    data = 1;
+                    return data;
+                break;
+                default:
+                    data = 0;
+                    return data;
+                break;
+            }
         }
 };
 
-class AudioMain: public Sensorstring {
+class Audio: public Sensorarray {
     public:
-        string getSensorstring(){
-            string data = "0011";
-            return data;
-        }
-};
-class ObjectId: public Sensorstring {
-    public:
-        string getSensorstring(){
-            string data = "branch";
-            return data;
+        int* getSensorarray(){
+            if (pos[0] == 2 || pos[0] == 3 || pos[0] == 4){
+                int a[9] = {1,1,1,0,0,0,0,0,1};
+                return a;
+            }
+            if (pos[1] == 2 || pos[1] == 3 || pos[1] == 4){
+                int b[9] = {1,1,1,0,0,0,0,1,0};
+                return b;
+            }
+            if (pos[0] == 27 || pos[0] == 28 || pos[0] == 29){
+                int c[9] = {1,1,1,0,0,0,0,1,1};
+                return c;
+            }
+            if (pos[1] == 27 || pos[1] == 28 || pos[1] == 29){
+                int d[] = {1,1,1,0,0,0,1,0,0};
+                return d;
+            }
+            else{
+                int e[] = {1,1,1,0,0,0,1,1,0};
+                return e;
+            }
         }
 };
 
@@ -59,11 +125,9 @@ int main(void)
     Ultraa Ula;
     Ultrab Ulb;
     Wasser Was;
-    AudioMain Audm;
-    ObjectId OID;
+    Audio Aud;
     cout << "U0 " << Ula.getSensor();
     cout << "\nU1 " << Ulb.getSensor();
     cout << "\nW0 " << Was.getSensor();
-    cout << "\nA0 " << Audm.getSensorstring();
-    cout << "\nObjectID " << OID.getSensorstring();
+    cout << "\nA ";
 }
