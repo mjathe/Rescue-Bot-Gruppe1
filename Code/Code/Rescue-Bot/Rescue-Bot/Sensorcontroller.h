c5d3c5c74dcd69d9589fd1db19d7e0f5ce86e5b9
JonasGerken <43642021+JonasGerken@users.noreply.github.com>
2020-08-17 13:05:55 +0200

    Update Sensorcontroller.h

--- Code/Code/Rescue-Bot/Rescue-Bot/Sensorcontroller.h
+++ Code/Code/Rescue-Bot/Rescue-Bot/Sensorcontroller.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <new>
 
 using namespace std;
 
@@ -17,17 +18,17 @@ class Sensorcontroller
 	float x3, y3, z3;	//acceleration
 	float orientationToNorth;
 	float orientation;
-	int ultrasonicVL, ultrasonicVR, ultrasonicHL, ultrasonicHR;
+	int ultrasonicVL, ultrasonicVR;
 
 
-	bool[] audioSignals[9];
-	bool[] audiData[4];
+	int[] audioSignals[9];
+	int[] audiData[4];
 	bool water;
-	bool[] obstacle[4];
+	int[] obstacle[2]
 
 
 
-	public bool[] readAudioSensors()
+	public int[] readAudioSensors()
 	{
 	//AudioOne{1,1,1,0,0,0,0,0,1} northwest
 	//AudioTwo{1,1,1,0,0,0,0,1,0}northeast
@@ -60,72 +61,40 @@ class Sensorcontroller
         		return audiData;
       	}
 
-	public float gyroscope(orientation, x1, y1, z1)
+	public float gyroscope()
 	{
+		orientation = readGyroscope(x1, y1, z1);
 
-		float orientation = readGyroscope(x1, y1, z1);
 		return orientation;
-
 	}
 
-	public float compass(orientationToNorth, x2, y2, z2)
+	public float compass()
 	{
-
-    		float orientationToNorth = readMagneticField(x2, y2, z2);
+    		orientationToNorth = readMagneticField(x2, y2, z2);
+		
     		return orientationToNorth;
-
 	}
-
-	public float acceleration(speed, x3, y3, z3)
-	{
-
-		float speed = readAcceleration(x3, y3, z3);
-		return speed;
-
-	}
-	/*
-	Aufruf:
-		Sensorcontroller.checkUltrasonicSensors(29,27); 
-		Sensorcontroller.checkUltrasonicsensors(29,27,25,23);
-
-
-	public int[] checkUltrasonicsensors(int ultrasonicVL,int ultrasonicVR,int ultrasonicHL,int ultrasonicHR)
-	{
-
-
-		 ultrasonicVL = readDigitalValue(ultrasonicPin1);
-		 ultrasonicVR = readDigitalValue(ultrasonicPin2);
-		 ultrasonicHL = readDigitalValue(ultrasonicPin3);
-		 ultrasonicHR = readDigitalValue(ultrasonicPin4);
-
-
-		return ?new? { ultrasonicVL, ultrasonicVR,ultrasonicHL, ultrasonicHR };
-	}*/
-	/*public int[] checkUltrasonicsensors(int ultrasonicVL,int ultrasonicVR)
-{
-
-
-	 ultrasonicVL = readDigitalValue(ultrasonicPin1);
-	 ultrasonicVR = readDigitalValue(ultrasonicPin2);
-
-
-	return ??new?? { ultrasonicVL,ultrasonicVR };
-}  */
-	public int[] checkUltrasonicsensors() 
-	{
 		
 
-		int  ultrasonicVL = readDigitalValue(ultrasonicPin1);
-		int  ultrasonicVR = readDigitalValue(ultrasonicPin2);
-
-
-
-		return ?new?{ultrasonicVL,ultrasonicVR};
+	public float acceleration()
+	{
+		speed = readAcceleration(x3, y3, z3);
+		
+		return speed;
 	}
 
+	public int[] checkUltrasonicsensors() 
+	{		
+		ultrasonicVL = readDigitalValue(ultrasonicPin1);
+		ultrasonicVR = readDigitalValue(ultrasonicPin2);
+		
+		return new int[]{ultrasonicVL,ultrasonicVR};
+	}
+		
 	public int checkwater() 
 	{
 		int water = readWater(watersens);
+		
 		return water;
 	}
 }
