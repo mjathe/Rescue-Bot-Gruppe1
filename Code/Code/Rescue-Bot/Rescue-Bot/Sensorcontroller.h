b8b8c99f801482a4ece937a7308e02849d63b4da
LordFlonni <43642299+LordFlonni@users.noreply.github.com>
2020-08-17 15:29:02 +0200

    Sensorcontroller.h systax korrekturen

--- Code/Code/Rescue-Bot/Rescue-Bot/Sensorcontroller.h
+++ Code/Code/Rescue-Bot/Rescue-Bot/Sensorcontroller.h
@@ -6,29 +6,38 @@ using namespace std;
 class Sensorcontroller
 {
 
-	#define ultrasonicPin1 = 29;
-	#define ultrasonicPin2 = 27;
-	#define ultrasonicPin3 = 25;
-	#define ultrasonicPin4 = 23;
+	#define ultrasonicPin1 29;
+	#define ultrasonicPin2 27;
+	#define ultrasonicPin3 25;
+	#define ultrasonicPin4  23;
 
-	#define watersens = 20;
+	#define watersens  20;
 
+
+private:
 	float x1, y1, z1;	//Gyroscope
 	float x2, y2, z2;	//compass
 	float x3, y3, z3;	//acceleration
 	float orientationToNorth;
 	float orientation;
-	int ultrasonicVL, ultrasonicVR;
+	int ultrasonicVL; ultrasonicVR;
 
 
-	int[] audioSignals[9];
-	int[] audiData[4];
+	int audioSignals[9];
+	int audiData[4];
 	bool water;
-	int[] obstacle[2]
+	int obstacle[2];
+
+public:
+	int[] readAudioSensors();
+	float compass();
+	float gyroscope();
+	float acceleration();
+	int[] checkUltraSonicSensors();
+	int checkWater();
 
 
-
-	public int[] readAudioSensors()
+	 int[] readAudioSensors()
 	{
 	//AudioOne{1,1,1,0,0,0,0,0,1} northwest
 	//AudioTwo{1,1,1,0,0,0,0,1,0}northeast
@@ -36,23 +45,26 @@ class Sensorcontroller
 	//Audiofour{1,1,1,0,0,0,1,0,0} Southeast shortway
     		for ( i=0; i< 9; i++)
     		{
-      			audioSignals[i] = readSensor(microfone)
-      			sleep(100) // zeit zwischen den bits
+				audioSignals[i] = readSensor(microfone);
+				sleep(100); // zeit zwischen den bits
     		}
-    		for(i= 0; i<3;i++)
-    		{
-      			if(audioSignals.subArray[0,5] == new {1,1,1,0,0,0})
-        	{
-         	switch(audioSignals.subArray[6,8]);
-         		case {001}: audioData[0]  = true;
-        			break;
-        		case {010}: audiData[1] = true;
-         			break;
-         		case {011}: audioData[2]  = true;
-        			break;
-         		case {100}: audiData[3] = true;
-         			break;
-        	}
+			for (i = 0; i < 3; i++)
+			{
+				if (audioSignals.subArray[0, 5] == new {1, 1, 1, 0, 0, 0})
+				{
+					switch (audioSignals.subArray[6, 8])
+					{
+					case {001}: audioData[0] = true;
+						break;
+					case {010}: audiData[1] = true;
+						break;
+					case {011}: audioData[2] = true;
+						break;
+					case {100}: audiData[3] = true;
+						break;
+					}
+				}
+			}
 
         		else
         		{
@@ -61,14 +73,14 @@ class Sensorcontroller
         		return audiData;
       	}
 
-	public float gyroscope()
+	 float gyroscope()
 	{
 		orientation = readGyroscope(x1, y1, z1);
 
 		return orientation;
 	}
 
-	public float compass()
+	 float compass()
 	{
     		orientationToNorth = readMagneticField(x2, y2, z2);
 		
@@ -76,14 +88,14 @@ class Sensorcontroller
 	}
 		
 
-	public float acceleration()
+	 float acceleration()
 	{
 		speed = readAcceleration(x3, y3, z3);
 		
 		return speed;
 	}
 
-	public int[] checkUltrasonicsensors() 
+	int[] checkUltraSonicSensors() 
 	{		
 		ultrasonicVL = readDigitalValue(ultrasonicPin1);
 		ultrasonicVR = readDigitalValue(ultrasonicPin2);
@@ -91,7 +103,7 @@ class Sensorcontroller
 		return new int[]{ultrasonicVL,ultrasonicVR};
 	}
 		
-	public int checkwater() 
+	 int checkWater() 
 	{
 		int water = readWater(watersens);
 		
