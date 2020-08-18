b40333e15553ceb12974bca0941f2794b16a7b22
Smiley <43641640+PSGSmiley@users.noreply.github.com>
2020-08-18 15:11:00 +0200

    Update Interface_Sensors.cpp

--- Code/Code/Rescue-Bot/Interface_Sensors.cpp
+++ Code/Code/Rescue-Bot/Interface_Sensors.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 char identify = 'R';
 int pos[3] ={28,3};
+int turm = 1;
 class Sensor {
     public:
         virtual int getSensor() = 0;
@@ -97,19 +98,19 @@ class Audio: public Sensorarray {
     public:
         string getSensorarray(){
             string a = "1,1,1,0,0,0,1,1,0";
-            if (pos[0] == 2 || pos[0] == 3 || pos[0] == 4){
+            if ((pos[0] == 2 || pos[0] == 3 || pos[0] == 4) && trum == 1){
                 a = "1,1,1,0,0,0,0,0,1";
                 return a;
             }
-            if (pos[1] == 2 || pos[1] == 3 || pos[1] == 4){
+            if ((pos[1] == 2 || pos[1] == 3 || pos[1] == 4) && trum == 2){
                 a = "1,1,1,0,0,0,0,1,0";
                 return a;
             }
-            if (pos[0] == 27 || pos[0] == 28 || pos[0] == 29){
+            if ((pos[0] == 27 || pos[0] == 28 || pos[0] == 29) && trum == 3){
                 a = "1,1,1,0,0,0,0,1,1";
                 return a;
             }
-            if (pos[1] == 27 || pos[1] == 28 || pos[1] == 29){
+            if ((pos[1] == 27 || pos[1] == 28 || pos[1] == 29) && trum == 4){
                 a = "1,1,1,0,0,0,1,0,0";
                 return a;
             }
